849f9b01cc39f97cb6e187804a319b5cf1b82988
Airat <airatyls@mail.ru>
2025-07-08 15:19:21 +0300

    задание 155

--- internship/src/main/java/org/airat/travel/insurance/core/repositories/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package org.airat.travel.insurance.core.repositories;
-
-import org.airat.travel.insurance.core.domain.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country,Integer> {
-    Country findByTitle(String title);
-}
